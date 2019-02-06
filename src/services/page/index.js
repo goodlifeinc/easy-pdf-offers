f6c872ae1b78d0757d2f9cb17fb4bc99dada1313
goodlifeinc <genata93@gmail.com>
2019-02-06 18:37:04 +0200

    headles chrome no sandbox

--- src/services/page/index.js
+++ src/services/page/index.js
@@ -6,7 +6,7 @@ class Page {
   }
 
   async pdf() {
-    const browser = await Puppeteer.launch();
+    const browser = await Puppeteer.launch({args: ['--no-sandbox'], headless: true});
     const page = await browser.newPage();
     await page.setContent(this.html);
 
