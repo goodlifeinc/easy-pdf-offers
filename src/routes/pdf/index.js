d1c51137531e234789ead9f0c62b4d52c5448026
goodlifeinc <genata93@gmail.com>
2019-02-06 17:55:37 +0200

    try catch

--- src/routes/pdf/index.js
+++ src/routes/pdf/index.js
@@ -9,34 +9,39 @@ const routes = [{
   method: 'GET',
   path: '/pdf',
   async handler(request, h) {
-    const firstPageTemplate = fs.readFileSync(path.join(__dirname, '../../services/renderer/templates/default/index.mustache.html'), 'utf8');
-    const pageTemplate = fs.readFileSync(path.join(__dirname, '../../services/renderer/templates/default/page.mustache.html'), 'utf8');
-    const model = JSON.parse(fs.readFileSync(path.join(__dirname, '../../services/renderer/templates/default/model.json')));
-
-    const unique = `${(new Date()).getTime()}`;
-
-    const dirname = path.join(__dirname, '../../output', `${unique}`);
-    const filename = 'offer.pdf';
-    const destinationFilePath = path.join(dirname, filename);
-    fse.ensureFileSync(destinationFilePath);
-
-    const rendererInstance = new Renderer(firstPageTemplate, pageTemplate);
-
-    const fileNames = await rendererInstance.render(model, unique);
-
-    const mergerInstance = new Merger(fileNames, destinationFilePath);
-    let output = 'error';
     try {
-      output = await mergerInstance.merge();
+      const firstPageTemplate = fs.readFileSync(path.join(__dirname, '../../services/renderer/templates/default/index.mustache.html'), 'utf8');
+      const pageTemplate = fs.readFileSync(path.join(__dirname, '../../services/renderer/templates/default/page.mustache.html'), 'utf8');
+      const model = JSON.parse(fs.readFileSync(path.join(__dirname, '../../services/renderer/templates/default/model.json')));
+
+      const unique = `${(new Date()).getTime()}`;
+
+      const dirname = path.join(__dirname, '../../output', `${unique}`);
+      const filename = 'offer.pdf';
+      const destinationFilePath = path.join(dirname, filename);
+      fse.ensureFileSync(destinationFilePath);
+
+      const rendererInstance = new Renderer(firstPageTemplate, pageTemplate);
+
+      const fileNames = await rendererInstance.render(model, unique);
+
+      const mergerInstance = new Merger(fileNames, destinationFilePath);
+      let output = 'error';
+      try {
+        output = await mergerInstance.merge();
+      } catch (e) {
+        console.log('error');
+      }
+      const file = fs.readFileSync(output);
+
+      fse.removeSync(dirname);
+
+      return h.response(file).header('Content-Disposition', `attachment; filename=${filename}`);
     } catch (e) {
-      console.log('error');
+      console.log(e);
+      return e;
     }
-    const file = fs.readFileSync(output);
-
-    fse.removeSync(dirname);
-
-    return h.response(file).header('Content-Disposition', `attachment; filename=${filename}`);
-  },
+  }
 }];
 
 module.exports = routes;
